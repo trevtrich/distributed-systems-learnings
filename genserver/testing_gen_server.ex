82eb729d8b5596a2561772027c107633ec77d401
Trevor Richardson <richardson.trev@gmail.com>
2023-11-28 08:06:07 -0600

    first genserver shoppinglist add/remove support

--- genserver/testing_gen_server.ex
+++ genserver/testing_gen_server.ex
@@ -1,12 +1,32 @@
-defmodule TestingGenServer do
-  @moduledoc false
-
+defmodule ShoppingList do
   use GenServer
 
+  # Client
   def start_link() do
     GenServer.start_link(__MODULE__, [])
   end
+  def add(pid, item) do
+      GenServer.cast(pid, item)
+  end
+  def remove(pid, item) do
+      GenServer.cast(pid, {:remove, item})
+  end
+  def view(pid) do
+      GenServer.call(pid, :view)
+  end
 
+  # Server
+  def handle_cast({:remove, item}, list) do
+      updated_list = Enum.filter(list, fn(x) -> x != item end)
+      {:noreply, updated_list}
+  end
+  def handle_cast(item, list) do
+      updated_list = [item|list]
+      {:noreply, updated_list}
+  end
+  def handle_call(:view, _from, list) do
+      {:reply, list, list}
+  end
   def init(list) do
     {:ok, list}
   end
